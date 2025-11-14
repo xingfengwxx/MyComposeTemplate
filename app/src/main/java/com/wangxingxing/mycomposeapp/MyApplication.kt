1e3a3dc996eccbd2c3b18b7a56677a1cdf8b424e
xingfengwxx <xingfengwxx@gmail.com>
2025-11-14 13:36:35 +0800

    添加Utils库

--- app/src/main/java/com/wangxingxing/mycomposeapp/MyApplication.kt
+++ app/src/main/java/com/wangxingxing/mycomposeapp/MyApplication.kt
@@ -1,8 +1,15 @@
 package com.wangxingxing.mycomposeapp
 
 import android.app.Application
+import com.blankj.utilcode.util.Utils
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MyApplication : Application()
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        Utils.init(this)
+    }
+}
 
